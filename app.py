18880da20fa58cd0b0333b3ce45c0e1d52eb19dd
maticel <mateysz.celinski@gmail.com>
2021-01-11 19:04:57 +0100

    11.01.2021

---
+++ app.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+
+import setup
+
+
+def main():
+    ## Prepare data
+    ## Create Model
+    ## Train Model
+    ## Test Model
+    pass
+
+
+def parse_args():
+    parser = ArgumentParser(description='')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main()
---
+++ setup.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).absolute().resolve().parent
+
+
+
